6475cb569b32817fe832e3bc6525dfa24a28f8d9
machariamarigi <machariamarigi@gmail.com>
2019-10-15 14:29:44 +0300

    initial flash template and styles

--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Flash } from '../flash.model';
 
 @Component({
   selector: 'app-flash',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flash.component.scss']
 })
 export class FlashComponent implements OnInit {
+  @Input() flash: Flash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No reaction ;)',
+    show: false
+  };
 
   constructor() { }
 
